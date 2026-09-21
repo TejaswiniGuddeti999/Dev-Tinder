dfba1534e6a06f9c41c5488ded9ce933717f9225
Tejaswini Guddeti <gtejaswini999@gmail.com>
2026-09-21 22:31:39 +0530

    error-handling

--- src/app.js
+++ src/app.js
@@ -3,26 +3,25 @@ const express = require('express');
 const app = express();
 
 
-const {adminAuth,userAuth} = require("./middlewares/auth.js");
-
-app.use("/admin", adminAuth);
-
-app.get("/user",userAuth, (req,res) => {
-    res.send("USer Data Sent");
+app.use("/", (err, req, res, next) => {
+    if (err) {
+        res.status(500).send("Something went wrong");
+}
 });
 
-app.get("/user/login", (req,res) => {
-    res.send("User Logged In");
-});
+app.get("/getUserDAta", (req, res) => {
+    try {
+        // logic
+        throw new Error("Something went wrong");
+        console.log("HEre is user data")
+    }
+    catch (err) {
+        res.send("Please Contact Support Team");
+    }
+   });
 
 
-app.get("/admin/getAllData", (req,res) => {
-    res.send("All data Sent");
-});
 
-app.get("/admin/deleteUser", (req,res) => {
-    res.send("User deleted");
-});
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
