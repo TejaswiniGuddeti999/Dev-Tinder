553d69234b4b2dec363a243c1d53dd0b5717769e
Tejaswini Guddeti <gtejaswini999@gmail.com>
2026-09-23 18:56:31 +0530

    dynamic data in signip API

--- src/app.js
+++ src/app.js
@@ -7,20 +7,18 @@ const app = express();
 
 const User = require("./models/user");
 
+app.use(express.json());
+
 app.post("/signup" ,async (req,res) => {
+    
     // creating a new instance of the user model
-    const user = new User({
-        firstName : "Sachin",
-        lastName : "Tendulkar",
-        emaiId : "someacricketer@gmail.com",
-        password: "nihcas"
-    });
+    const user = new User(req.body);
     try {
         await user.save();
-    res.send("User added successfully")
+        res.send("User added successfully")
     }
     catch (err) {
-        res.status(400).setDefaultEncoding("error saving the user:", err);
+        res.status(400).send("error saving the user:" + err.message);
     }
     
 });
@@ -33,7 +31,7 @@ connectDB()
         });
     })
     .catch((err) => {
-        console.log("Database cannot be connected due to the following error: ", err);
+        console.log("Database cannot be connected due to the following error: " + err.message);
     });
 
 
--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 
 const uri = process.env.MONGO_URI;
+console.log("Connecting to:", uri);
+
 
 const mongoose = require("mongoose");
 
